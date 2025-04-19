5584b7eba412e186f5ec43694c0c3fa78d6a3b47
Maksimod <maksumonka@mail.com>
2025-04-19 14:55:02 +0300

    better

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,11 @@
 module.exports = {
-  apps: [{
-    name: "speech-translator",
-    script: "node_modules/nodemon/bin/nodemon.js",
-    args: "server.js",
-    instances: 1,
-    autorestart: true,
-    watch: true,
-    ignore_watch: ["node_modules", "public", "temp"],
-    max_memory_restart: "1G",
-    env: {
-      NODE_ENV: "development"
-    }
-  }]
-} 
+    apps: [{
+      name: "my-app",
+      script: "server.js",
+      watch: true,
+      ignore_watch: ["node_modules"],
+      env: {
+        "NODE_ENV": "development"
+      }
+    }]
+  }
